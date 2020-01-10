c7fbd9aaae9b1e95ef3748c1765c5485fb674e78
James Strayer <strayerjja@gmail.com>
2020-01-10 05:28:33 -0500

    final correction

--- DataScript.R
+++ DataScript.R
@@ -3,8 +3,8 @@ if(!require(tidyverse)){
   library(tidyverse)
 }
 #Importing data
-DailySales<-read_csv("Data/DailySales.csv")
-View(DailySales)
+DailySales<-read_csv("https://raw.githubusercontent.com/jjamesSt/CapestoneProject/master/Data/DailySales.csv")
+glimpse(DailySales)
 #Removing unnecessary column
 DataForAnalysis<-DailySales%>%select(-Weekday,-Month,-Day,-`annee fiscale`)
 #Add a column for weekday and month
@@ -21,11 +21,12 @@ DataForAnalysis[is.na(DataForAnalysis$Bar_Sales),]$Bar_Sales<-0
 DataForAnalysis[is.na(DataForAnalysis$Retail),]$Retail<-0
 DataForAnalysis[is.na(DataForAnalysis$TakeOutSales),]$TakeOutSales<-0
 DataForAnalysis[is.na(DataForAnalysis$Sales_Restaurant),]$Sales_Restaurant<-0
-
-#Creation of the data frame of bank holidays and other event (such as Valentine's Day), to identify them
-Event<-c('2015-04-06','2015-05-10','2015-05-18','2015-06-21','2015-06-24','2015-07-01','2015-09-07','2015-10-12','2015-10-31','2015-11-11','2015-12-25','2016-01-01','2016-02-14','2016-03-17','2016-03-28','2016-05-08','2016-05-23','2016-06-19','2016-06-24','2016-07-01','2016-09-05','2016-10-10','2016-10-31','2016-11-11','2016-12-25','2017-01-01','2017-02-14','2017-03-17','2017-04-17','2017-05-14','2017-05-22','2017-06-18','2017-06-24','2017-07-01','2017-09-04','2017-10-09','2017-10-31','2017-11-11','2017-12-24','2018-01-01','2018-02-14','2018-03-17','2018-04-02','2018-04-13','2018-05-21','2018-06-17','2018-06-24','2018-07-01','2018-09-03','2018-10-08','2018-10-31','2018-11-11','2018-12-25','2019-01-01','2019-02-14','2019-03-17','2019-04-22','2019-05-12','2019-05-20','2019-06-16','2019-06-24','2019-07-01','2019-10-02','2019-10-14','2019-10-31','2019-11-11','2019-12-25')
-#Add a column for EventDay
-DataForAnalysis<-DataForAnalysis%>%mutate(EventDay=ifelse(Date %in% Event,TRUE,FALSE))
+#look at the structure of the data
+glimpse(DataForAnalysis)
+#filter for 2016,2017,2018 and 2019
+DataForAnalysis<-DataForAnalysis%>%filter(Year>=2016)
+#Summary statistics for the variable
+summary(DataForAnalysis)
 #Distribution of Sales by Year
 DataForAnalysis%>%group_by(Year)%>%ggplot(aes(x=Date,y=Sales,group=Year))+geom_boxplot()+ggtitle("Sales distribution by Year")
 #Distribution of sales by day of the week faceted by Year
@@ -60,23 +61,20 @@ zoo_train<-train_set%>%select(-Weekday,-Month,-Year,-EventDay,-Retail,-TakeOutSa
 ts_train<-ts(zoo_train,start=c(2016,1),end=c(2018,1),frequency=52)
 #Show the time serie
 plot(ts_train)
+labs(title = "Sales Times Series by year")
 #Build ARIMA model
 #package for ARIMA model
 if(!require(forecast)){
   install.packages("forecast")
 }
 library(forecast)
-#Broom-style tidiers for forecast package
-if(!require(sweep)){
-  install.packages("sweep")
-}
-library(sweep) 
 #Build ARIMA model
+set.seed(412,sample.kind = "Rounding")
 fit_arima<-auto.arima(ts_train)
 summary(fit_arima)
 #see the actual value with the prediction
 sw_augment(fit_arima)
-#plot the forecast
+#plot the residuals of the models
 sw_augment(fit_arima, timetk_idx = TRUE) %>% 
   ggplot(aes(x = index, y = .resid)) +
   geom_point(colour = 'blue', size = 2) + 
@@ -114,12 +112,13 @@ error_arima<- fcast_tbl %>% filter(key == 'forecast') %>%
 error_arima
 #transform Data to time series for linear regression
 DataLr<-DataForAnalysis%>%tk_augment_timeseries_signature()
-DataLr
 #Define train and test set
 trainLr_set<-DataLr%>%filter(Date<'2019-01-01')
 trainLr_set<-trainLr_set%>% select(-Retail,-TakeOutSales,-Bar_Sales,-Sales_Restaurant,-EventDay)
 testLr_set<-DataLr%>%filter(Date>='2019-01-01')
 testLr_set<-testLr_set%>% select(-Retail,-TakeOutSales,-Bar_Sales,-Sales_Restaurant,-EventDay)
+str(trainLr_set)
+str(testLr_set)
 #construct the model
 set.seed(42,sample.kind = "Rounding")
 fit_Lr<-lm(Sales~.,data=select(trainLr_set,-Date))
